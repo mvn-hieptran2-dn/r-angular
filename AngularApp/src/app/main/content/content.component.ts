e9cb7be3bae096448545896b746ccd65ae0733ba
hieptran <hiep.tran2@asiantech.vn>
2018-05-16 21:42:22 +0700

    create data service member.json

--- AngularApp/src/app/main/content/content.component.ts
+++ AngularApp/src/app/main/content/content.component.ts
@@ -1,6 +1,6 @@
 import { Component, Output, Input } from '@angular/core';
 import { InfoMember } from '../../listinfo';
-import { posts } from '../../mock-listinfo'
+import { posts } from '../../mock-listinfo';
 
 @Component ({
   selector: 'page-content',
@@ -18,9 +18,7 @@ export class PageContentComponent {
   onSelect(lst : InfoMember): void {
     this.selectedPost = lst;
     this.isShowDetail = true;
-    
     this.listpost = this.articles.filter(item => item.Position === lst.Position && lst !== item);
-    // console.log(this.listpost);
   }
 
   onBack() {
