35c0e5d30342447189c8d92f0c3868513e5e091b
hieptran <hiep.tran2@asiantech.vn>
2018-05-19 19:34:34 +0700

    edit css form login

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
