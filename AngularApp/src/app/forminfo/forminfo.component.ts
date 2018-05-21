ccc7ba6816ec10fb2a7bd3491fcc9722ae8d2f9e
hieptran <hiep.tran2@asiantech.vn>
2018-05-21 22:10:23 +0700

    validators form info

--- AngularApp/src/app/forminfo/forminfo.component.ts
+++ AngularApp/src/app/forminfo/forminfo.component.ts
@@ -8,5 +8,26 @@ import { FormGroup, FormControl,  Validators } from '@angular/forms';
 })
 
 export class FormInforComponent  {
-  
+  FormModel: any;
+  submittedForm: any;
+  CertificateDiv: boolean;
+
+  constructor() {
+    this.FormModel = new FormGroup({
+      name: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+      ]),
+      code: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(6),
+        Validators.pattern("^(AT)[0-9]{4}$")
+      ]),
+      skill: new FormControl('', Validators.required),
+      citizen: new FormControl('', Validators.required),
+      certificate: new FormControl('', Validators.required),
+    })
+  }
 }
